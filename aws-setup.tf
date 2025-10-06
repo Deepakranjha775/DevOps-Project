1fd96ffda43f3c890a35973184c62a230b8bc0c4
Deepak Ranjha <deepak.ranjha@kyndryl.com>
2025-10-06 16:19:47 +0530

    terraform pipeline

--- aws-setup.tf
+++ aws-setup.tf
@@ -64,6 +64,8 @@ resource "aws_instance" "web" {
   instance_type = "t3.micro"
   subnet_id              = aws_subnet.main.id
   vpc_security_group_ids = [aws_security_group.instance_sg.id]
+  key_name               = "devops-project"
+  associate_public_ip_address = true
 
   tags = {
     Name = "DevOps Project"
